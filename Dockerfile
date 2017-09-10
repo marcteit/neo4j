2bce7f6ce3fe68542a8f2395e8767b68d4b0cd63
Marc Teitelbaum <marcteit@gmail.com>
2017-09-10 00:21:06 -0300

    Using relative path

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM neo4j:enterprise
 
-ADD set-config.sh /var/lib/neo4j/
-RUN chmod +x /var/lib/neo4j/set-config.sh
+ADD set-config.sh .
+RUN chmod +x set-config.sh
 
 CMD ["./set-config.sh"]
--- set-config.sh
+++ set-config.sh
@@ -2,14 +2,14 @@
 
 sed -i -e \
   "s|^#dbms.mode=CORE$|dbms.mode=CORE|" \
-  /var/lib/neo4j/conf/neo4j.conf
+  conf/neo4j.conf
 
 sed -i -e \
   "s|^#causal_clustering.expected_core_cluster_size=.*$|causal_clustering.expected_core_cluster_size=$expected_core_cluster_size|" \
-  /var/lib/neo4j/conf/neo4j.conf
+  conf/neo4j.conf
 
 sed -i -e \
   "s|^#causal_clustering.initial_discovery_members=.*$|causal_clustering.initial_discovery_members=$initial_discovery_members|" \
-  /var/lib/neo4j/conf/neo4j.conf
+  conf/neo4j.conf
 
 /docker-entrypoint.sh neo4j
